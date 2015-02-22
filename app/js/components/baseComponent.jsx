a64ad7fffc7d702fb8460c309b03b7108b54a20d
alex <alexandre.enehc@gmail.com>
2015-02-22 20:32:00 +0100

    :art: add mt to title && color nuance on selected language

--- app/js/components/baseComponent.jsx
+++ app/js/components/baseComponent.jsx
@@ -88,13 +88,15 @@ var Component = React.createClass({
         <div className="flex flex-col resp-h full desktop">
           <header className="flex flex-col flex-3 center">
             <div className="flex-1">
-              <h1 className="mt0 mb0 getsub-red">Getsub!</h1>
+              <h1 className="mt1 mb0 getsub-red">Getsub!</h1>
               <p className="h2 mt0 mb2 light dark-mid-grey">A better way to find your subtitles</p>
             </div>
           </header>
 
           <section className="flex flex-align-center flex-just-center flex-2 mxa col-4">
-            <button className="rounded btn-flat dark col-10" onClick={this.handleClick}>Select your language</button>
+            <button className="rounded btn-flat dark col-10" onClick={this.handleClick}>
+              Select your language
+            </button>
           </section>
 
           <div className="flex flex-7 flex-align-center flex-just-center center mt1 mb3 col-8 mxa dashed sh-inner dark-mid-grey" onDrop={this.onHandleDrop} onDragLeave={this.onHandleDragLeave} onDragOver={this.onHandleDragOver}>
